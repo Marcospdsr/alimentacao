6f8d618ebdae233e7bca908c375732ba3d6c6df1
Marcospdsr <marcospdsr@hotmail.com>
2018-05-26 11:39:29 -0300

    acertar redirecionamento de realizar pedidos

--- src/pages/pedido/pedido.ts
+++ src/pages/pedido/pedido.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LanchesPage } from '../lanches/lanches';
 import { AcompanhamentosPage } from '../acompanhamentos/acompanhamentos';
-
+import { ConfirmarPedidoPage } from "../confirmar-pedido/confirmar-pedido";
 import { BebidasPage } from '../bebidas/bebidas';
 import { PratosPage } from '../pratos/pratos';
 
@@ -29,5 +29,8 @@ export class PedidoPage {
   }goToPratos(params){
     if (!params) params = {};
     this.navCtrl.push(PratosPage);
+  }goToConfirmarPedido(params){
+    if (!params) params = {};
+    this.navCtrl.push(ConfirmarPedidoPage);
   }
 }
