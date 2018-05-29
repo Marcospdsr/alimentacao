4867e8b8e8c3c80bffd7e1a1651f3b6f2a6e9bb7
Marcospdsr <marcospdsr@hotmail.com>
2018-05-29 09:40:33 -0300

    adição do ngForm na tela de login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,9 @@
+import { User } from './../../Models/user';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HomePage } from '../home/home';
+
+
 
 @Component({
   selector: 'page-login',
@@ -8,9 +11,12 @@ import { HomePage } from '../home/home';
 })
 export class LoginPage {
 
+  public login = new User();
+
   constructor(public navCtrl: NavController) {
   }
   goToHome(params){
+    console.log('email: ' + this.login.email + ' senha: ' + this.login.senha)
     if (!params) params = {};
     this.navCtrl.push(HomePage);
   }
